fd0b6f2665c71118b4636ca2a5a98e8ebb954f34
Pavel <48092890+Kioshilol@users.noreply.github.com>
2021-10-02 15:10:57 +0300

    Add files via upload

---
+++ DiscordFunnyBot/DiscordFunnyBot/Commands/FunCommands.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+
+namespace DiscordFunnyBot.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("hello"), Description("say hello")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Yo guys");
+        }
+    }
+}
---
+++ DiscordFunnyBot/DiscordFunnyBot/Models/ConfigModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordFunnyBot.Models
+{
+    public struct ConfigModel
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
